5f3ef219040afe910b060f5107771bb5e3e58192
Junior Paula <luke.junnior@icloud.com>
2022-09-10 16:17:18 -0300

    test: change tests extrategy with jest fn

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,37 +1,26 @@
-import { LoadFacebookUserApi } from '@/data/interfaces/apis';
 import { FacebookAuthenticationService } from '@/data/services';
 import { AuthenticationError } from '@/domain/errors';
 
-class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
-  token?: string;
-  result = undefined;
-  callsCount = 0;
-
-  async loadUser(
-    params: LoadFacebookUserApi.Params,
-  ): Promise<LoadFacebookUserApi.Result> {
-    this.token = params.token;
-    this.callsCount++;
-    return this.result;
-  }
-}
-
 describe('FacebookAuthentication Service', () => {
   test('Should call LoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy();
+    const loadFacebookUserApiSpy = {
+      loadUser: jest.fn(),
+    };
     const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy);
-
     await sut.perform({ token: 'any_token' });
 
-    expect(loadFacebookUserApiSpy.token).toBe('any_token');
-    expect(loadFacebookUserApiSpy.callsCount).toBe(1);
+    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledWith({
+      token: 'any_token',
+    });
+    expect(loadFacebookUserApiSpy.loadUser).toHaveBeenCalledTimes(1);
   });
 
   test('Should return AuthenticationError with LoadFacebookUserApi returns undefined', async () => {
-    const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy();
-    loadFacebookUserApiSpy.result = undefined;
+    const loadFacebookUserApiSpy = {
+      loadUser: jest.fn(),
+    };
+    loadFacebookUserApiSpy.loadUser.mockResolvedValueOnce(undefined);
     const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy);
-
     const authResult = await sut.perform({ token: 'any_token' });
 
     expect(authResult).toEqual(new AuthenticationError());
