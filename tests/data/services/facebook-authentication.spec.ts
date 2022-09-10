9c042b2d0e13e2880282c25b38201ca123034868
Junior Paula <luke.junnior@icloud.com>
2022-09-10 16:30:48 -0300

    test: change tests extrategy with jest-mock-extended

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,11 +1,12 @@
+import { mock } from 'jest-mock-extended';
 import { FacebookAuthenticationService } from '@/data/services';
 import { AuthenticationError } from '@/domain/errors';
+import { LoadFacebookUserApi } from '@/data/interfaces/apis';
 
 describe('FacebookAuthentication Service', () => {
   test('Should call LoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApiSpy = {
-      loadUser: jest.fn(),
-    };
+    const loadFacebookUserApiSpy = mock<LoadFacebookUserApi>();
+
     const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy);
     await sut.perform({ token: 'any_token' });
 
@@ -16,9 +17,8 @@ describe('FacebookAuthentication Service', () => {
   });
 
   test('Should return AuthenticationError with LoadFacebookUserApi returns undefined', async () => {
-    const loadFacebookUserApiSpy = {
-      loadUser: jest.fn(),
-    };
+    const loadFacebookUserApiSpy = mock<LoadFacebookUserApi>();
+
     loadFacebookUserApiSpy.loadUser.mockResolvedValueOnce(undefined);
     const sut = new FacebookAuthenticationService(loadFacebookUserApiSpy);
     const authResult = await sut.perform({ token: 'any_token' });
