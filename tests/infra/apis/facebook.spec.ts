c45f3177a4022cb120f93f5906d454255bed988a
Junior Paula <luke.junnior@icloud.com>
2022-09-13 19:09:39 -0300

    test: ensure FacebookApi gep app token with correct params

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -8,18 +8,26 @@ export interface HttpClient {
 export namespace HttpClient {
   export type Params = {
     url: string;
+    params: object;
   };
 }
 
 describe('FacebookApi', () => {
+  const clientId = 'any_client_id';
+  const clientSecret = 'any_client_secret';
   test('Should get app token', async () => {
     const httpClient = mock<HttpClient>();
-    const sut = new FacebookApi(httpClient);
+    const sut = new FacebookApi(httpClient, clientId, clientSecret);
 
     await sut.loadUser({ token: 'any_client_token' });
 
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oauth/access_token',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials',
+      },
     });
   });
 });
