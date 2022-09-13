cf86720fe143a3155d453bef9e0cd51278ec2a3c
Junior Paula <luke.junnior@icloud.com>
2022-09-13 20:44:26 -0300

    feat: ensure AxiosHttpClient return data on success

--- src/http/axios-client.ts
+++ src/http/axios-client.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { HttpClient } from '@/infra/http';
 
 export class AxiosHttpClient {
-  async get(args: HttpClient.Params): Promise<void> {
-    await axios.get(args.url, { params: args.params });
+  async get(args: HttpClient.Params): Promise<any> {
+    const result = await axios.get(args.url, { params: args.params });
+    return result.data;
   }
 }
