cf4121b9a96c71e87db7fdf66aad5a98c60a3643
Junior Paula <luke.junnior@icloud.com>
2022-09-13 19:09:24 -0300

    feat: ensure FacebookApi gep app token with correct params

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -3,11 +3,20 @@ import { HttpClient } from './facebook.spec';
 
 export class FacebookApi {
   private readonly baseUrl = 'https://graph.facebook.com';
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string,
+  ) {}
 
   async loadUser(parmas: LoadFacebookUserApi.Params): Promise<void> {
     await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'client_credentials',
+      },
     });
   }
 }
