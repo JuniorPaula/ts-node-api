579c6298e82fe5d09ff59044aa40b2c1fb95861f
Junior Paula <luke.junnior@icloud.com>
2022-09-13 20:44:41 -0300

    test: ensure AxiosHttpClient return data on success

--- tests/http/axios-client.spec.ts
+++ tests/http/axios-client.spec.ts
@@ -14,6 +14,10 @@ describe('AxiosHttpClient', () => {
     url = 'any_url';
     params = { any: 'any' };
     fakeAxios = axios as jest.Mocked<typeof axios>;
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data',
+    });
   });
 
   beforeEach(() => {
@@ -25,5 +29,11 @@ describe('AxiosHttpClient', () => {
 
     expect(fakeAxios.get).toHaveBeenCalledWith(url, { params });
     expect(fakeAxios.get).toHaveBeenCalledTimes(1);
+  });
+
+  test('Should return data on success', async () => {
+    const result = await sut.get({ url, params });
+
+    expect(result).toEqual('any_data');
   });
 });
