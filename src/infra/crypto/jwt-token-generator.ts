d9733d5e7ea4c9ffcc399c71070d74bac99d7bb9
Junior Paula <luke.junnior@icloud.com>
2022-09-15 14:20:19 -0300

    feat: ensure JwtTokenGenerator return token on success

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -4,10 +4,14 @@ import { TokenGenerator } from '@/data/interfaces/crypto';
 export class JwtTokenGenerator {
   constructor(private readonly secret: string) {}
 
-  async generateToken(params: TokenGenerator.Params): Promise<void> {
+  async generateToken(
+    params: TokenGenerator.Params,
+  ): Promise<TokenGenerator.Result> {
     const expirationInSeconds = params.expireIn / 1000;
-    jwt.sign({ key: params.key }, this.secret, {
+    const token = jwt.sign({ key: params.key }, this.secret, {
       expiresIn: expirationInSeconds,
     });
+
+    return token;
   }
 }
