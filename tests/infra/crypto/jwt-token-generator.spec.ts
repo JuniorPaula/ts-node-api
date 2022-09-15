811b37d1ecc3df0b00b3b18b6f24154cc80db3d8
Junior Paula <luke.junnior@icloud.com>
2022-09-15 14:15:26 -0300

    refactor: move sut creation to a beforeEach

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -5,10 +5,18 @@ import jwt from 'jsonwebtoken';
 jest.mock('jsonwebtoken');
 
 describe('JwtTokenGenerator', () => {
-  test('Should call sign() with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>;
-    const sut = new JwtTokenGenerator('any_secrect');
+  let sut: JwtTokenGenerator;
+  let fakeJwt: jest.Mocked<typeof jwt>;
 
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>;
+  });
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('any_secrect');
+  });
+
+  test('Should call sign() with correct params', async () => {
     await sut.generateToken({ key: 'any_key', expireIn: 1000 });
 
     expect(fakeJwt.sign).toHaveBeenCalledWith(
