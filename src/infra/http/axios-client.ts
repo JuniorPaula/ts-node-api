7f101556a23588337af6489763759e3db587a916
Junior Paula <luke.junnior@icloud.com>
2022-09-17 08:35:51 -0300

    refactor: ensure AxiosHttpClient implements HttpCleint

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { HttpClient } from './client';
 
-export class AxiosHttpClient {
-  async get(args: HttpClient.Params): Promise<any> {
+export class AxiosHttpClient implements HttpClient {
+  async get<T = any>(args: HttpClient.Params): Promise<T> {
     const result = await axios.get(args.url, { params: args.params });
     return result.data;
   }
