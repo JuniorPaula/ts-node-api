7918d3f7d4d60ab537269e2a66d25162439d7ad5
Junior Paula <luke.junnior@icloud.com>
2022-09-18 15:06:16 -0300

    refactor: move inMemoryDb creation to its own file

--- tests/infra/postgres/repositories/user-account.spec.ts
+++ tests/infra/postgres/repositories/user-account.spec.ts
@@ -1,19 +1,9 @@
-import { IBackup, IMemoryDb, newDb } from 'pg-mem';
+import { IBackup } from 'pg-mem';
 import { getConnection, getRepository, Repository } from 'typeorm';
 
 import { PgUserAccountRepository } from '@/infra/postgres/repositories';
 import { PgUser } from '@/infra/postgres/entities';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
-  const db = newDb();
-  const connection = await db.adapters.createTypeormConnection({
-    type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts'],
-  });
-  await connection.synchronize();
-  return db;
-};
+import { makeFakeDb } from '@/tests/infra/postgres/mocks';
 
 describe('PgUserAccountRepository', () => {
   describe('load()', () => {
